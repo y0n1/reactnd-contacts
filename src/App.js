d9b55777c14e4816afce0354a160e9b7ee70bf36
y0n1 <y0n1@outlook.com>
2017-07-03 04:49:31 +0300

    refactor: Adds persistency with ContactsAPI

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import React, { Component } from 'react'
 import ContactsList from './ContactsList'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
+    contacts: []
+  }
+
+  componentDidMount() {
+    (async function getAllAsync() { 
+      try {
+        const contacts = await ContactsAPI.getAll()
+        this.setState({contacts})
+      } catch (error) {
+        alert(error.message)
       }
-    ]
+    }).bind(this)()
   }
 
   handleRemoveContact(contact) {
     const contactId = _contact => _contact.id !== contact.id
     this.setState({contacts: this.state.contacts.filter(contactId)})
+    ContactsAPI.remove(contact)
   }
   handleRemoveContact = this.handleRemoveContact.bind(this)
 
