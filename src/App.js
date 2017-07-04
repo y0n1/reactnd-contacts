3f03609aaf9ce73464b84e6feed06e76aef4751a
y0n1 <y0n1@outlook.com>
2017-07-05 02:08:35 +0300

    refactor: Implements create contact feat.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,68 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ContactsList from './ContactsList'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact'
 
 class App extends Component {
   state = {
-    screen: 'list',
     contacts: []
   }
 
   componentDidMount() {
-    (async function getAllAsync() { 
+    async function getAllAsync() { 
       try {
         const contacts = await ContactsAPI.getAll()
         this.setState({contacts})
       } catch (error) {
         alert(error.message)
       }
-    }).bind(this)()
+    }
+
+    return getAllAsync.bind(this)()
   }
 
   handleRemoveContact(contact) {
-    const contactId = _contact => _contact.id !== contact.id
-    this.setState({contacts: this.state.contacts.filter(contactId)})
-    ContactsAPI.remove(contact)
+    const byContactId = _contact => _contact.id !== contact.id
+    this.setState({contacts: this.state.contacts.filter(byContactId)})
+    
+    async function removeAsync() {
+      try {
+        ContactsAPI.remove(contact)
+      } catch (error) {
+        alert(error)
+      }
+    }
+    
+    removeAsync()
   }
   handleRemoveContact = this.handleRemoveContact.bind(this)
 
+  handleCreateContact(history) {
+    async function createContactAsync(contact) {
+      try {
+        ContactsAPI.create(contact)
+        this.setState((prevState) => ({
+          contacts: prevState.contacts.concat([contact])
+        }))
+        history.push('/')
+      } catch (error) {
+        alert(error.message)
+      }
+    }
+
+    return createContactAsync.bind(this)
+  }
+  handleCreateContact = this.handleCreateContact.bind(this)
+  
   render() {
+    const _ContactList_ = () => (<ContactsList onRemoveContact={this.handleRemoveContact} contacts={this.state.contacts}/>)
+    const _CreateContact_ = ({history}) => (<CreateContact onCreateContact={this.handleCreateContact(history)}/>)
     return (
-        <div className="app">
-          {this.state.screen === 'list' && (
-            <ContactsList 
-              onNavigate={() => {
-                this.setState({screen: 'create'})
-              }}
-              onRemoveContact={this.handleRemoveContact}
-              contacts={this.state.contacts}
-            />)}
-          {this.state.screen === 'create' && (
-            <CreateContact/>
-          )}
-        </div>
+      <div className="app">
+        <Route path="/" exact render={_ContactList_}></Route>
+        <Route path="/create" render={_CreateContact_}></Route>
+      </div>
     )
   }
 }
